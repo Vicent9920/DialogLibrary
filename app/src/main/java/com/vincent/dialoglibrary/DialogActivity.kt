e642217d5816bf7b6b5a8f2023e8b2357854ad0e
vincent <weixing9920@163.com>
2019-07-02 23:14:26 +0800

    升级到androidX

--- app/src/main/java/com/vincent/dialoglibrary/DialogActivity.kt
+++ app/src/main/java/com/vincent/dialoglibrary/DialogActivity.kt
@@ -3,18 +3,18 @@ package com.vincent.dialoglibrary
 import android.app.Dialog
 import android.os.Bundle
 import android.os.Handler
-import android.support.v4.app.FragmentActivity
-import android.support.v7.app.AppCompatActivity
+import android.os.Looper
 import android.view.Gravity
 import android.view.View
 import android.widget.Toast
-import com.hjq.dialog.*
-import com.hjq.dialog.base.BaseDialog
-import com.hjq.dialog.base.BaseDialogFragment
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+import com.vincent.dialoglibrary.base.BaseDialog
+import com.vincent.dialoglibrary.base.BaseDialogFragment
 import kotlinx.android.synthetic.main.activity_dialog.*
 
 
-class DialogActivity : AppCompatActivity() {
+ class DialogActivity : AppCompatActivity() {
 
     lateinit var mToast: Toast
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,25 +26,30 @@ class DialogActivity : AppCompatActivity() {
 
     private fun initEvent(){
         btn_dialog_message.setOnClickListener {
-            MessageDialog.Builder(this)
+            MessageDialog.Builder(this )
                 .setTitle("我是标题") // 标题可以不用填写
                 .setMessage("我是内容")
                 .setConfirm("确定")
                 .setCancel("取消") // 设置 null 表示不显示取消按钮
                 //.setAutoDismiss(false) // 设置点击按钮后不关闭对话框
-                .setListener(object : MessageDialog.OnListener {
-                    override fun confirm(dialog: Dialog?) {
-                        toast("确定了")
-                    }
+                .apply {
+                    this.mListener = object : com.vincent.dialoglibrary.MessageDialog.OnListener {
+                        override fun confirm(dialog: Dialog) {
+                            toast("确定了")
+                        }
 
-                    override fun cancel(dialog: Dialog?) {
-                        toast("取消了")
+                        override fun cancel(dialog: Dialog) {
+                            toast("取消了")
+                        }
+
+
                     }
-                })
+                }
+
                 .show()
         }
         btn_dialog_bottom_menu.setOnClickListener {
-            val data = mutableListOf<String>()
+            val data = ArrayList<String>()
             for (i in 0..9) {
                 data.add("我是数据$i")
             }
@@ -52,22 +57,25 @@ class DialogActivity : AppCompatActivity() {
                 .setCancel("取消") // 设置 null 表示不显示取消按钮
                 //.setAutoDismiss(false) // 设置点击按钮后不关闭对话框
                 .setList(data)
-                .setListener(object : MenuDialog.OnListener {
-                    override fun select(dialog: Dialog?, position: Int, text: String?) {
-                        toast("位置：$position，文本：$text")
-                    }
-
-                    override fun cancel(dialog: Dialog?) {
-                        toast("取消了")
-                    }
-
-                })
                 .setGravity(Gravity.BOTTOM)
-                .setAnimStyle(BaseDialog.AnimStyle.BOTTOM)
+                .apply {
+                    mListener = object : MenuDialog.OnListener {
+                        override fun select(dialog: Dialog, position: Int, text: String) {
+                            toast("位置：$position，文本：$text")
+                        }
+
+                        override fun cancel(dialog: Dialog) {
+                            toast("取消了")
+                        }
+
+
+                    }
+                    mAnimations = BaseDialog.AnimStyle.BOTTOM
+                }
                 .show()
         }
         btn_dialog_center_menu.setOnClickListener {
-            val data = mutableListOf<String>()
+            val data = ArrayList<String>()
             for (i in 0..9) {
                 data.add("我是数据$i")
             }
@@ -75,18 +83,21 @@ class DialogActivity : AppCompatActivity() {
                 .setCancel(null) // 设置 null 表示不显示取消按钮
                 //.setAutoDismiss(false) // 设置点击按钮后不关闭对话框
                 .setList(data)
-                .setListener(object : MenuDialog.OnListener {
-                    override fun select(dialog: Dialog?, position: Int, text: String?) {
-                        toast("位置：$position，文本：$text")
-                    }
-
-                    override fun cancel(dialog: Dialog?) {
-                        toast("取消了")
-                    }
-
-                })
                 .setGravity(Gravity.CENTER)
-                .setAnimStyle(BaseDialog.AnimStyle.BOTTOM)
+                .apply {
+                    mListener = object : MenuDialog.OnListener {
+                        override fun select(dialog: Dialog, position: Int, text: String) {
+                            toast("位置：$position，文本：$text")
+                        }
+
+                        override fun cancel(dialog: Dialog) {
+                            toast("取消了")
+                        }
+
+
+                    }
+                    mAnimations = BaseDialog.AnimStyle.SCALE
+                }
                 .show()
         }
         btn_dialog_succeed_toast.setOnClickListener {
@@ -108,11 +119,10 @@ class DialogActivity : AppCompatActivity() {
                 .show()
         }
         btn_dialog_wait.setOnClickListener {
-            val time:Long = 3000
             val dialog = WaitDialog.Builder(this)
                 .setMessage("加载中...") // 消息文本可以不用填写
                 .show()
-            Handler().postDelayed({ dialog.dismiss() },time)
+            Handler(Looper.getMainLooper()).postDelayed({ dialog.dismiss() }, 3000)
         }
         btn_dialog_pay.setOnClickListener {
             PayPasswordDialog.Builder(this)
@@ -120,24 +130,34 @@ class DialogActivity : AppCompatActivity() {
                 .setSubTitle("用于购买一个女盆友")
                 .setMoney("￥ 100.00")
                 //.setAutoDismiss(false) // 设置点击按钮后不关闭对话框
-                .setListener(object : PayPasswordDialog.OnListener {
-                    override fun complete(dialog: Dialog?, password: String?) {
-                        toast(password)
-                    }
+                .apply {
+                    this.mListener = object : PayPasswordDialog.OnListener {
+                        override fun complete(dialog: Dialog, password: String) {
+                            toast(password)
+                        }
 
-                    override fun cancel(dialog: Dialog?) {
-                        toast("取消了")
+                        override fun cancel(dialog: Dialog) {
+                            toast("取消了")
+                        }
                     }
-
-                })
+                }
                 .show()
         }
         btn_dialog_date.setOnClickListener {
+            DateDialog.Builder(this).apply { this.mListener = object :DateDialog.OnListener{
+                override fun onSelected(dialog: Dialog, year: Int, month: Int, day: Int) {
+                    toast("${year}年${month}月${day}日")
+                }
 
+                override fun onCancel(dialog: Dialog) {
+                    toast("取消")
+                }
+
+            } }.setTitle("请选择日期").show()
         }
         btn_dialog_custom.setOnClickListener {
+
             Custom(this)
-                .setAnimStyle(BaseDialog.AnimStyle.SCALE)
                 .setContentView(R.layout.dialog_custom)
                 //.setText(id, "我是预设置的文本")
                 .setOnClickListener(R.id.btn_dialog_custom_ok, object : BaseDialog.OnClickListener<View> {
@@ -146,9 +166,19 @@ class DialogActivity : AppCompatActivity() {
                     }
 
                 })
+                .apply {
+                    this.mAnimations = BaseDialog.AnimStyle.SCALE
+                }
                 .show()
+
+
         }
+
+
     }
+
+    class Custom(activity: AppCompatActivity, themeResId: Int = -1):
+        BaseDialogFragment.Builder<Custom>(activity, themeResId)
 
     private fun toast(info:String?){
         info?:return
@@ -156,6 +186,5 @@ class DialogActivity : AppCompatActivity() {
         mToast.show()
     }
 
-    class Custom(activity: FragmentActivity, themeResId: Int = -1):
-        BaseDialogFragment.Builder<Custom>(activity, themeResId)
+
 }
